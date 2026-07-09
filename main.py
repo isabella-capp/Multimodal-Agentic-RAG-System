a1afd5b7bdce46e5a7b4f88b42d4354d652e19cf
Isabella Cappellino <315503@studenti.unimore.it>
2026-07-09 19:32:05 +0200

    ci: update pyproject

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from cvcs2026!")
+import faiss
+import json
+base_folder = "/work/cvcs2026/encyclopedic"
 
 
 if __name__ == "__main__":
-    main()
+    # Carica solo il JSON, l'indice FAISS lo lasciamo stare per ora
+    with open(f"{base_folder}/encyclopedic_kb_wiki.json", "r") as f:
+        # Usiamo un iteratore o leggiamo solo un pezzo se fosse una lista enorme
+        data = json.load(f)
+
+    print(f"Tipo di struttura: {type(data)}")
+    print(f"{json.dumps(data[:2], indent=2)}")  # Stampa solo i primi 1000 caratteri per non saturare il terminale
